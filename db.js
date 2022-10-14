fa909dcae638b01114a4ea64e5d34d810d47c5e9
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-10-14 20:01:14 +0530

    set-up environment variables

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const URI = 'mongodb://localhost:27017/spend_adani_money_db';
-// const URI = 'mongodb+srv://tanishk:learn_new_things_daily@cluster0.bth5loo.mongodb.net/spend_adani_money_db?retryWrites=true&w=majority';
-const URI = process.env.REACT_APP_ATLAS_URI;
+const URI = process.env.ATLAS_URI;
 
 const connectToMongo = async () => {
     mongoose.connect(URI, () => {
